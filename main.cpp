6c7c7d76946c9c245e0a887cba8373124595aa0f
Michael-Schneider <Mschneider8@student.rccd,edu>
2023-02-13 15:56:07 -0800

    First commit to 17C repo

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/* 
+ * File:   main.cpp
+ * Author: Michael Schneider
+ * Student ID: 2508577
+ * Created on August 24, 2022, 7:19 PM
+ */
+
+#include <iostream>
+
+int main() 
+{
+    std::cout << "hello world \n";
+    return 0;
+}
+
